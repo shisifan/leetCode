1f19e2127b1276d427ae64dc86b1212e08419ed4
shisifan <1449348331@qq.com>
2021-10-26 21:57:30 +0800

    10.26

--- promise.allSettled().js
+++ promise.allSettled().js
@@ -1,25 +1,20 @@
 function allSettled(promises) {
-    if (promises.length === 0) {
-        return Promise.resolve([])
+  let result = [];
+  let len = promises.length;
+
+  return new Promise((resolve, reject) => {
+    len || reject(promises);
+    for (let i = 0; i < len; i++) {
+      Promise.resolve(promises[i])
+        .then((value) => {
+          result[i] = { status: "fulfilled", value };
+        })
+        .catch((reason) => {
+          result[i] = { status: "rejected", reason };
+        })
+        .finally(() => {
+          t.length === len && resolve(result);
+        });
     }
-    let result = [];
-    let index = 0;
-    let len = promises.length;
-    return new Promise((resolve) => {
-        for(let i = 0; i < len; i++) {
-            Promise.resolve((promises[i]))
-            .then(value => {
-                result[i] = {status: 'fulfilled', value}
-            })
-            .catch(reason => {
-                result[i] = {status: 'rejected', reason};
-            })
-            .finally(() => {
-                index++;
-                if(index === len) {
-                    resolve(result);
-                }
-            })
-        }
-    })
+  });
 }
